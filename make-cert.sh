c4c3595fc1f0f5d2700886ed9508f098ac394e57
Herman Ras <herman.ras.it@gmail.com>
2025-05-15 12:31:38 +0200

    bug fix

--- make-cert.sh
+++ make-cert.sh
@@ -18,19 +18,19 @@ else
     else
     echo "making cert for $domain"
     echo "cert will be droped in $pwd/certs"
-    mkdir $pwd/certs/$domain
+    mkdir "$pwd/certs/$domain"
     docker run --rm --name servercerts \
-      -v $pwd/certs:/certs \
+      -v ./certs:/certs \
       -e CA_SUBJECT="lab-ca.com" \
       -e CA_EXPIRE=1825 \
       -e SSL_EXPIRE=365 \
-      -e SSL_KEY=/certs/$domain/$domain.key \
-      -e SSL_CERT=/certs/$domain/$domain.pem \
-      -e SSL_CSR=/certs/$domain/$domain.csr \
+      -e SSL_KEY=./certs/$domain/$domain.key \
+      -e SSL_CERT=./certs/$domain/$domain.pem \
+      -e SSL_CSR=./certs/$domain/$domain.csr \
       -e SSL_IP=127.0.0.1 \
       -e SSL_DNS=$domain \
       -e SSL_SUBJECT=$domain \
       superseb/omgwtfssl
-    touch $pwd/certs/$domain/$date.txt
+    touch "$pwd/certs/$domain/$date.txt"
     fi
 fi
